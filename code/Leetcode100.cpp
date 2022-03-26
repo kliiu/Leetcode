60d40091955fc3d8a8f1268f9237522b1f0af57f
‘kliiu’ <klliiuuu@outlook.com>
2022-03-26 16:51:24 +0800

    "add files"

--- code/Leetcode100.cpp
+++ code/Leetcode100.cpp
@@ -9,6 +9,7 @@
 #include <unordered_set>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
 struct ListNode {
     int val;
@@ -328,6 +329,197 @@ int lengthOfLongestSubstring(string s) {
     }
     return max;
 }
+//5. 最长回文子串
+string longestPalindrome(string s) {
+    if(s.size()<2)return s;
+    int n=s.size();
+    int maxlen=1;//n>=1
+    int begin=0;
+    vector<vector<int>> dp(n,vector<int>(n));//dp[i][j]表示s[i:j]是否是回文串
+    for(int i=0;i<n;i++)
+    {
+        dp[i][i]=true;//一个字符为回文串
+    }
+    for(int L=2;L<=n;L++)//L为串的长度
+    {//左值为i，右值为j,L=j-i+1;
+        for(int i=0;i<n;i++)
+        {
+            int j=L+i-1;
+            if(j>=n)break;//不符合条件，右边界越界
+
+            if(s[j]==s[i])//若对应值相等
+            {   if(L-1<4)
+                dp[i][j]=true;//当对应值相等且长度小于4，说明为回文串
+                else dp[i][j]=dp[i+1][j-1];//否则若字串为回文串则为回文串
+            }
+            else dp[i][j]=false;
+
+            //判断回文
+            if(dp[i][j]==true&&L>maxlen)
+            {
+                maxlen=L;
+                begin=i;
+            }
+
+        }
+
+    }
+    return s.substr(begin, maxlen);
+
+}
+//11. 盛最多水的容器
+class Solution_11 {
+public:
+    int maxArea_TLE(vector<int>& height) {
+        int max=0;
+        for(int i =0;i<height.size();i++)
+        {
+            for(int j=i+1;j<height.size();j++)
+            {   int min=height[i]>height[j]?height[j]:height[i];
+                if((j-i)*min>max)max=(j-i)*min;
+            }
+        }
+        return max;
+    }
+
+    int maxArea(vector<int>& height) {
+        int max=0;
+        int i=0;int j=height.size()-1;
+        while(i!=j)
+        {
+            int min=height[i]>height[j]?height[j]:height[i];
+            if((j-i)*min>max)max=(j-i)*min;
+            if(height[i]>height[j])j--;
+            else i++;
+        }
+        return max;
+    }
+};
+
+//三数之和
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> ans;
+    std::sort(nums.begin(), nums.end());
+    for(int i=0;i<nums.size();i++)
+    {   if(i>0&&nums[i]==nums[i-1])
+            continue;//跳到与上一个元素不等的元素
+        int third=nums.size()-1;//应-1
+        for(int j=i+1;j<nums.size();j++)
+        {   if(j>i+1&&nums[j]==nums[j-1])//跳到与上一个元素不等的元素
+                continue;
+
+            while(third>j&&nums[i]+nums[j]+nums[third]>0)
+            {
+                third--;//保证j在third右边
+            }
+            if(third==j)break;
+            if(nums[i]+nums[j]+nums[third]==0)
+            {
+                vector<int> temp;
+                temp.push_back(nums[i]);
+                temp.push_back(nums[j]);
+                temp.push_back(nums[third]);
+                ans.push_back(temp);
+            }
+        }
+    }
+    return  ans;
+}
+//17. 电话号码的字母组合
+class Solution_17 {
+public:
+    vector<string> letterCombinations(string digits) {
+        //map映射字母和数字
+        unordered_map<char,vector<string>> phone;
+        phone['2']={"a","b","c"};
+        phone['3']={"d","e","f"};
+        phone['4']={"g","h","i"};
+        phone['5']={"j","k","l"};
+        phone['6']={"m","n","o"};
+        phone['7']={"p","q","r","s"};
+        phone['8']={"t","u","v"};
+        phone['9']={"w","x","y","z"};
+
+        vector<string>ans;
+        for(auto i:digits )
+        {   //对每个数字循环
+            if(ans.empty())
+            {
+                ans=phone[i];//第一个字母直接全部push
+                continue;
+            }
+            //int size=phone[i].size();//当前数字对应几个数字
+            vector<string> temp=ans;
+            ans.clear();
+            for(auto j:temp)
+            {
+                for (auto k: phone[i])
+                {//对每个数字对应的字母都要出现
+                    ans.push_back(j+k);
+                }
+            }
+        }
+        return ans;
+    }
+
+};
+//19. 删除链表的倒数第 N 个结点
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd_1(ListNode* head, int n) {
+        //不是直接删？
+        ListNode* current=head;
+        int cnt=0;
+        while(current)//
+        {
+            current=current->next;
+            cnt++;//计算共有多少个节点
+        }
+
+        int del=cnt-n;//要删除的节点位置
+        if(del==0)return head->next;
+        current=head;
+        while(del-1>0)//移动到前一个时停止
+        {
+            current=current->next;
+            del--;
+        }
+
+        //if(!current->next)return  nullptr;//若要被删除的点不存在
+        //else
+        current->next=current->next->next;
+        return head;
+    }
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        //快慢指针
+        ListNode* fast=head;
+        ListNode* slow=head;
+        int cnt=0;
+        while(current)//
+        {
+            current=current->next;
+            cnt++;//计算共有多少个节点
+        }
+
+        int del=cnt-n;//要删除的节点位置
+        if(del==0)return head->next;
+        current=head;
+        while(del-1>0)//移动到前一个时停止
+        {
+            current=current->next;
+            del--;
+        }
+
+        //if(!current->next)return  nullptr;//若要被删除的点不存在
+        //else
+        current->next=current->next->next;
+        return head;
+
+    }
+
+};
+
 
     int main()
 {
@@ -354,7 +546,8 @@ int lengthOfLongestSubstring(string s) {
     l2=head->next;
     ListNode* ans=addTwoNumbers(l1,l2);
     */
-    string a="abba";
-    int b= lengthOfLongestSubstring(a);
+
+    string a="23";
+    auto ans=letterCombinations(a);
     return 0;
 }
